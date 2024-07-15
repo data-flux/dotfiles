de98d03645707261f8e3102154e7fab6fc61ae70
Dirk Meijer <data-flux@users.noreply.github.com>
2024-07-15 16:11:58 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,25 @@ config.font = wezterm.font 'RecMonoDuotone'
 config.font_size = 10.0
 config.line_height = 1.0
 config.window_background_opacity = 0.80
+
 config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+    { 
+        key = 'PageUp',
+        mods = 'CTRL',
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = 'PageDown',
+        mods = 'CTRL',
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = 'T',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+}
 
 config.default_prog = { '/usr/bin/fish' }
 
